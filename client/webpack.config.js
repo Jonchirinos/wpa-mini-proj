4c963d3c81f1c4e8df0d7813034b1fd77c87be3d
Jon Chirinos <chirinosjon@gmail.com>
2022-04-16 13:37:46 -0500

    up and running

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,27 +1,74 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { InjectManifest } = require("workbox-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js',
-      cards: './src/js/cards.js'
-    },
+    return {
+        mode: "development",
+        entry: {
+            main: "./src/js/index.js",
+            install: "./src/js/install.js",
+            cards: "./src/js/cards.js",
+        },
 
-    // TODO: Add the correct output
-    output: {
-      
-    },
+        // TODO: Add the correct output which creates dist folder
+        output: {
+            filename: "[name].bundle.js",
+            path: path.resolve(__dirname, "dist"),
+        },
 
-    // TODO: Add the correct plugins
-    plugins: [
-     
-    ],
+        // TODO: Add the correct plugins
+        plugins: [
+            // create HTML file for the dist
+            new HtmlWebpackPlugin({
+                template: "./index.html",
+                title: "TODOs List",
+            }),
+            // generate service worker
+            new InjectManifest({
+                swSrc: "./src-sw.js",
+                swDest: "src-sw.js",
+            }),
+            // create manifest.json
+            new WebpackPwaManifest({
+                name: "contact",
+                description: "contact directory where you can add contacts",
+                background_color: "olive",
+                theme_color: "navajowhite",
+                inject: true,
+                fingerprints: false,
+                start_url: "/",
+                publicPath: "/",
+                icons: [
+                    {
+                        src: path.resolve("src/images/logo.png"),
+                        sizes: [96, 128, 192, 256, 384, 512],
+                        destination: path.join("assets", "icons"),
+                    },
+                ],
+            }),
+        ],
 
-    // TODO: Add the correct modules
-    module: {
-
-    }
-  };
+        // TODO: Add the correct modules
+        module: {
+            rules: [
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env"],
+                            plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime"],
+                        },
+                    },
+                },
+                {
+                    test: /\.css$/i,
+                    use: ["style-loader", "css-loader"],
+                },
+            ],
+        },
+    };
 };
